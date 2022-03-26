f0bf8d5b0dd587563ca396822ad399d2951a535b
MuXiu1997 <muxiu1997@gmail.com>
2022-03-26 16:17:49 +0800

    Update parse_issue.py

--- parse_issue.py
+++ parse_issue.py
@@ -2,22 +2,20 @@ import sys
 
 from utils import set_output
 
+
+def set_output_and_print(key: str, value: str):
+    set_output(key, value)
+    print(f'{key}={value}')
+
+
 if __name__ == '__main__':
     issue_body = sys.argv[1]
     lines = issue_body.splitlines()
     old_modpack_url = lines[2]
-    set_output('old-modpack-url', old_modpack_url)
-    print(f'old-modpack-url: {old_modpack_url}')
     new_modpack_url = lines[6]
-    set_output('new-modpack-url', new_modpack_url)
-    print(f'new-modpack-url: {new_modpack_url}')
-    set_output('reference-branch', lines[10])
-    print(f'reference-branch: {lines[10]}')
-    set_output('branch', lines[14])
-    print(f'branch: {lines[14]}')
-    old_modpack_name = old_modpack_url.split('/')[-1]
-    set_output('old-modpack-name', old_modpack_name)
-    print(f'old-modpack-name: {old_modpack_name}')
-    new_modpack_name = new_modpack_url.split('/')[-1]
-    set_output('new-modpack-name', new_modpack_name)
-    print(f'new-modpack-name: {new_modpack_name}')
+    set_output_and_print('old-modpack-url', old_modpack_url)
+    set_output_and_print('new-modpack-url', new_modpack_url)
+    set_output_and_print('reference-branch', lines[10])
+    set_output_and_print('branch', lines[14])
+    set_output_and_print('old-modpack-name', old_modpack_url.split('/')[-1])
+    set_output_and_print('new-modpack-name', new_modpack_url.split('/')[-1])
