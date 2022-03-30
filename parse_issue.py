82687d14f099bcb3bd9f06f03113adb9088ca0e3
MuXiu1997 <muxiu1997@gmail.com>
2022-03-30 15:33:28 +0800

    Update parse_issue.py

--- parse_issue.py
+++ parse_issue.py
@@ -1,4 +1,6 @@
-import sys
+import json
+import os
+from os import path
 
 from utils import set_output
 
@@ -8,14 +10,31 @@ def set_output_and_print(key: str, value: str):
     print(f'{key}={value}')
 
 
+def parse_url(url) -> (str, str, str):
+    filename = url.split('/')[-1]
+    version = path.splitext(path.basename(filename))[0].split('-')[-1]
+    return url, filename, version
+
+
 if __name__ == '__main__':
-    issue_body = sys.argv[1]
-    lines = issue_body.splitlines()
-    old_modpack_url = lines[2]
-    new_modpack_url = lines[6]
+    issue = json.loads(os.environ.get('GITHUB_ISSUE'))
+    valid_label = os.environ.get('VALID_LABEL')
+    valid_user = os.environ.get('VALID_USER')
+
+    passed = 'false'
+    if any([label['name'] == valid_label for label in issue['labels']]) and issue['user']['login'] == valid_user:
+        passed = 'true'
+
+    lines = issue['body'].splitlines()
+    old_modpack_url, old_modpack_name, old_version = parse_url(lines[2])
+    new_modpack_url, new_modpack_name, new_version = parse_url(lines[6])
+
+    set_output_and_print('passed', passed)
     set_output_and_print('old-modpack-url', old_modpack_url)
     set_output_and_print('new-modpack-url', new_modpack_url)
+    set_output_and_print('old-modpack-name', old_modpack_name)
+    set_output_and_print('new-modpack-name', new_modpack_name)
+    set_output_and_print('old-version', old_version)
+    set_output_and_print('new-version', new_version)
     set_output_and_print('reference-branch', lines[10])
     set_output_and_print('branch', lines[14])
-    set_output_and_print('old-modpack-name', old_modpack_url.split('/')[-1])
-    set_output_and_print('new-modpack-name', new_modpack_url.split('/')[-1])
