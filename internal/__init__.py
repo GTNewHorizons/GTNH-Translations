53e20f0d206ae0d4f7a241e7f23f029f52930bdf
MuXiu1997 <muxiu1997@gmail.com>
2022-06-09 15:53:23 +0800

    Rework init

--- internal/__init__.py
+++
@@ -1,5 +0,0 @@
-from .modpack import ModPack
-from .translationpack import TranslationPack
-from .comparable import Comparable
-from .generator import ZH_DUPLICATE_KEYS, EN_DUPLICATE_KEYS, generate_translation
-from .issue import get_issue
