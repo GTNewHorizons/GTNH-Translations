acca53dd1357eb1fab72963bfe25a1e18b99509f
MuXiu1997 <muxiu1997@gmail.com>
2023-12-23 15:50:41 +0800

    test(utils): update test of `line_iterator`

--- tests/utils/test_line_iterator.py
+++ tests/utils/test_line_iterator.py
@@ -2,9 +2,11 @@ from gtnh_translation_compare.utils.line_iterator import line_iterator
 
 
 def test_line_iterator() -> None:
-    content = "\n".join(["", "test", ""])
+    content = "\n".join(["", "test", "", "测试", ""])
     result = list(line_iterator(content))
     assert result == [
         (0, "", 0, 0),
         (1, "test", 1, 5),
+        (2, "", 6, 6),
+        (3, "测试", 7, 9),
     ]
