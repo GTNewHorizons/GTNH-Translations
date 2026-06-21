6334dc7d10a20e24d1880fa4045fe98243f8a950
Anson Yeung <nanolify@gmail.com>
2026-06-22 01:59:03 +0800

    address comments

--- src/gtnh_translation_compare/paratranz/types.py
+++ src/gtnh_translation_compare/paratranz/types.py
@@ -61,10 +61,12 @@ class FileExtra(BaseModel):
                     logger.warning(f"FileExtra.{legacy_key} is deprecated, use FileExtra.target_relpath instead")
             if "enUsRelpath" in data:
                 data["en_us_relpath"] = data["enUsRelpath"]
-                logger.warning(f"Compatibility for enUsRelpath")
+                data.pop("enUsRelpath")
+                logger.warning("Compatibility for enUsRelpath")
             if "targetRelpath" in data:
                 data["target_relpath"] = data["targetRelpath"]
-                logger.warning(f"Compatibility for targetRelpath")
+                data.pop("targetRelpath")
+                logger.warning("Compatibility for targetRelpath")
         return data
 
 
