f2d026e73959dac918e76eab61b52c2caf4e220b
Anson Yeung <nanolify@gmail.com>
2026-06-22 20:08:09 +0800

    Line break fixes

--- src/gtnh_translation_compare/utils/line_break_subst.py
+++ src/gtnh_translation_compare/utils/line_break_subst.py
@@ -3,6 +3,9 @@ from typing import Optional
 
 from loguru import logger
 
+from gtnh_translation_compare import settings
+from gtnh_translation_compare.paratranz.types import File
+
 LINE_BREAK_CONTEXT_PREFIX = "@gtnh-line-break-form="
 LINE_BREAK_CONTEXT_NOOP = "LF"
 LINE_BREAK_CONTEXT_DECODE_MAP = {
@@ -15,16 +18,28 @@ LINE_BREAK_CONTEXT_DECODE_MAP = {
 }
 
 
-def line_break_subst(context: Optional[str], translation: str) -> str:
-  if context is None: return translation
+def get_line_break_symbol(dflt_setting: Optional[str], context: Optional[str]) -> str:
+  if dflt_setting is None:
+    default_symbol = LINE_BREAK_CONTEXT_NOOP
+  else:
+    default_symbol = dflt_setting
+  if context is None: return default_symbol
   sp_ctx: list[str] = re.split(r'\r?\n', context)
   for ctx_line in sp_ctx:
     if not ctx_line.startswith(LINE_BREAK_CONTEXT_PREFIX): continue
     norm_to_entry = ctx_line[len(LINE_BREAK_CONTEXT_PREFIX):]
-    if norm_to_entry == LINE_BREAK_CONTEXT_NOOP: return translation
+    if norm_to_entry == LINE_BREAK_CONTEXT_NOOP: return norm_to_entry
     if norm_to_entry not in LINE_BREAK_CONTEXT_DECODE_MAP:
       logger.warning(f"Not recognized for line break: {norm_to_entry}")
-      return translation
-    norm_to = LINE_BREAK_CONTEXT_DECODE_MAP[norm_to_entry]
-    return re.sub(r'\r?\n', norm_to, translation)
-  return translation
+      return default_symbol
+    return LINE_BREAK_CONTEXT_DECODE_MAP[norm_to_entry]
+  return default_symbol
+
+
+def line_break_subst(file: File, context: Optional[str], translation: str) -> str:
+  file_setting = None
+  if file.name == settings.GT_LANG_TARGET_REL_PATH + ".json":
+    file_setting = "<BR>"
+  sym = get_line_break_symbol(file_setting, context)
+  if sym == LINE_BREAK_CONTEXT_NOOP: return translation
+  return re.sub(r'\r?\n', sym, translation)
